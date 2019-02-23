c86ebb587a6fb15a7e0bcd001ed67a2cf8054907
hope2hopeOutman <3423593391@qq.com>
2019-02-23 08:05:02 -0800

    New GCC adaption.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # linux-0.11-dev
+
+Mainly study linux system and try to refact it for practice.
+
+主要工作：在较新的ubuntu平台，用相对较新的GCC编译0.11的代码，然后在bochs2.6.9中跑起来，具体做了哪些适配工作，请查看AdaptionListForNewerGCC.txt
+         引导程序和OS代码还是放在软盘中，但注意文件系统放在硬盘的第二分区了，所以build.c文件中的#define DEFAULT_MINOR_ROOT 2设置为2了，这点要注意。
+
---
+++ kernel/printk.c
@@ -0,0 +1,57 @@
+/*
+ *  linux/kernel/printk.c
+ *
+ *  (C) 1991  Linus Torvalds
+ */
+
+/*
+ * When in kernel-mode, we cannot use printf, as fs is liable to
+ * point to 'interesting' things. Make a printf with fs-saving, and
+ * all is well.
+ */
+#include <stdarg.h>
+#include <stddef.h>
+
+#include <linux/kernel.h>
+
+static char buf[1024];
+
+extern int vsprintf(char * buf, const char * fmt, va_list args);
+
+int printk(const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i=vsprintf(buf,fmt,args);
+	va_end(args);
+	__asm__("push %%fs\n\t"
+		"push %%ds\n\t"
+		"pop %%fs\n\t"
+		"pushl %0\n\t"
+		"pushl $buf\n\t"
+		"pushl $0\n\t"
+		"call tty_write\n\t"
+		"addl $8,%%esp\n\t"
+		"popl %0\n\t"
+		"pop %%fs"
+		::"r" (i):"ax","cx","dx");
+	return i;
+}
+
+char* cpy_str_to_kernel(char * dest,const char *src)
+{
+__asm__(
+	"push %%ds\n"
+	"push %%fs\n\t"
+	"pop  %%ds\n\t"
+	"cld\n\t"
+	"1:\tlodsb\n\t"
+	"stosb\n\t"
+	"testb %%al,%%al\n\t"
+	"jne 1b\n\t"
+	"pop %%ds"
+	::"S" (src),"D" (dest));
+return dest;
+}
